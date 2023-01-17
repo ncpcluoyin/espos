4acd2b70558421270af3bca2df301cfda4fbb5fa
luoyin <912271673@qq.com>
2023-01-17 11:26:20 +0800

    update

--- main/main.c
+++ main/main.c
@@ -3,7 +3,14 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+void sys_main()
+{
+}
+
 void app_main(void)
 {
-    xTaskCreatePinnedToCore();
+    for(;;)
+    {
+        printf("sb");
+    }
 }
