773517c16f20b0576c174ab93c43df7d2c976cb4
luoyin <912271673@qq.com>
2023-01-19 11:12:00 +0800

    update

--- main/main.c
+++ main/main.c
@@ -3,14 +3,19 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+void watchdog()
+{
+    for(;;){printf("hello\n");}
+}
+
 void sys_main()
 {
+    for(;;){printf("world\n");}
 }
 
 void app_main(void)
 {
-    for(;;)
-    {
-        printf("sb");
-    }
+    xTaskCreatePinnedToCore(sys_main,"SystemMainThread",8192,NULL,1,NULL,1);
+    xTaskCreatePinnedToCore(watchdog,"SystemWatchDog",4096,NULL,1,NULL,0);
+    for(;;){printf("!!!\n");}
 }
