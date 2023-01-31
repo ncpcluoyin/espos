e1ba460527d62a5ac43d9477b969f537b2c8c7d1
luoyin <912271673@qq.com>
2023-01-31 13:18:17 +0800

    update

---
+++ main/filesystem.h
@@ -0,0 +1,3 @@
+#ifndef _FILESYSTEM_H_
+#define _FILESYSTEM_H_
+#endif
--- main/main.c
+++ main/main.c
@@ -5,17 +5,14 @@
 
 void watchdog()
 {
-    for(;;){printf("hello\n");}
 }
 
 void sys_main()
 {
-    for(;;){printf("world\n");}
 }
 
 void app_main(void)
 {
     xTaskCreatePinnedToCore(sys_main,"SystemMainThread",8192,NULL,1,NULL,1);
-    xTaskCreatePinnedToCore(watchdog,"SystemWatchDog",4096,NULL,1,NULL,0);
-    for(;;){printf("!!!\n");}
+    xTaskCreatePinnedToCore(watchdog,"SystemWatchDog",8192,NULL,1,NULL,0);
 }
