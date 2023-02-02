6ae76d6226ae6f0be2771e1c2b2e8c25b587af22
luoyin <912271673@qq.com>
2023-02-02 22:26:52 +0800

    update

--- main/main.c
+++ main/main.c
@@ -2,17 +2,7 @@
 #include "filesystem.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-
-void watchdog()
-{
-}
-
-void sys_main()
-{
-}
+#include "filesystem.h"
 
 void app_main(void)
-{
-    xTaskCreatePinnedToCore(sys_main,"SystemMainThread",8192,NULL,1,NULL,1);
-    xTaskCreatePinnedToCore(watchdog,"SystemWatchDog",8192,NULL,1,NULL,0);
-}
+{}
