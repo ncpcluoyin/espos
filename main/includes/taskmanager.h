331745e67f1ee103fb2ae784b9313087adcc39f2
luoyin <912271673@qq.com>
2023-02-03 23:07:38 +0800

    update

--- main/includes/taskmanager.h
+++ main/includes/taskmanager.h
@@ -1,1 +1,16 @@
 #pragma once
+
+typedef short Priority;
+typedef void (*_Function_t)(void *);
+
+#define Priority_god 0
+#define Priority_system 1
+#define Priority_doge 2
+
+typedef struct
+{
+    const char * const task_name;
+    _Function_t _func_name;
+    Priority _priority;
+    short _complexity;
+} taskinfo;
--- main/main.cpp
+++ main/main.cpp
@@ -1,3 +1,16 @@
+#include "taskmanager.h"
+#include "message.h"
+#include "filesystem.h"
+#include <iostream>
+using namespace std;
+
+void hello(void *)
+{
+    cout << "a" << endl;
+    return;
+}
+
 extern "C" void app_main(void)
 {
+    taskinfo _taskinfo = {"hello",hello,Priority_doge,(short)114514};
 }
