624a7781248079fef0f119ef259397032cfb2429
luoyin <912271673@qq.com>
2023-02-03 17:46:59 +0800

    update

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,8 @@
-#include <stdio.h>
+#include <iostream>
 #include "filesystem.h"
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
 #include "filesystem.h"
 #include "message.h"
+#include "taskmanager.h"
 
 void app_main(void)
 {
---
+++ main/taskmanager.h
@@ -0,0 +1,12 @@
+#ifndef _TASKMANAGER_H_
+#define _TASKMANAGER_H_
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+typedef struct taskmanager_info_t
+{
+    TaskFunction_t
+};
+
+
+#endif
